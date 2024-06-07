af8cbc24908d660264e1b49bc94c616cf23388f4
kyoungmin <stephen4951@gmail.com>
2024-06-07 12:59:20 +0900

    #3. 로그인, 회원가입 화면 구분, 유효성 검증

--- components/Auth/AuthForm.js
+++ components/Auth/AuthForm.js
@@ -1,13 +1,20 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Input from '../ui/Input';
 import { useState } from 'react';
 import Button from '../ui/Button';
 
-const AuthForm = () => {
+const AuthForm = ({ isLogin, onSubmit, credentialsInvalid }) => {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredName, setEneteredName] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
   const [enteredComfirmPassword, setEnteredConfirmPassword] = useState('');
+
+  const {
+    email: emailIsValid,
+    name: nameIsValid,
+    password: passwordIsValid,
+    confirmPassword: passwordDontMatch,
+  } = credentialsInvalid;
 
   const updateInputValueHandler = (inputType, enteredValue) => {
     console.log('inputType: ', inputType);
@@ -30,6 +37,13 @@ const AuthForm = () => {
 
   const submitHandler = () => {
     console.log('버튼 클릭됨! submit!');
+    // 사용자의 입력 상태값을 객체로 포장해서 AuthContent에게 넘긴다 -> 부모쪽에서 유효성 검증 할 것.
+    onSubmit({
+      email: enteredEmail,
+      name: enteredName,
+      password: enteredPassword,
+      confirmPassword: enteredComfirmPassword,
+    });
   };
 
   return (
@@ -42,23 +56,40 @@ const AuthForm = () => {
           // bind에 제공되는 첫번째 인수는 곧 실행할 함수의 this 키워드로 설정됩니다.
           // 두 번째 인수는 지정한 함수에 전달할 값을 세팅하면 됩니다.
           onUpdateValue={updateInputValueHandler.bind(this, 'email')}
+          isInvalid={emailIsValid}
+          value={enteredEmail}
         />
-        <Input
-          label='이름'
-          onUpdateValue={updateInputValueHandler.bind(this, 'name')}
-        />
+        {!isLogin && (
+          <Input
+            label='이름'
+            onUpdateValue={updateInputValueHandler.bind(this, 'name')}
+            isInvalid={nameIsValid}
+            value={enteredName}
+          />
+        )}
         <Input
           label='비밀번호'
           secure
           onUpdateValue={updateInputValueHandler.bind(this, 'password')}
+          isInvalid={passwordIsValid}
+          value={enteredPassword}
         />
-        <Input
-          label='비밀번호확인'
-          secure
-          onUpdateValue={updateInputValueHandler.bind(this, 'confirmPassword')}
-        />
-        <View>
-          <Button onPress={submitHandler}>회원가입</Button>
+        {!isLogin && (
+          <Input
+            label='비밀번호확인'
+            secure
+            onUpdateValue={updateInputValueHandler.bind(
+              this,
+              'confirmPassword',
+            )}
+            isInvalid={passwordDontMatch}
+            value={enteredComfirmPassword}
+          />
+        )}
+        <View style={styles.buttons}>
+          <Button onPress={submitHandler}>
+            {isLogin ? '로그인' : '회원 가입'}
+          </Button>
         </View>
       </View>
     </View>
@@ -66,3 +97,9 @@ const AuthForm = () => {
 };
 
 export default AuthForm;
+
+const styles = StyleSheet.create({
+  buttons: {
+    marginTop: 12,
+  },
+});
